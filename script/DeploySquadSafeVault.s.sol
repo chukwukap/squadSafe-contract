a43c904e735de61c9871b2ff47c821b3784af1a1
Chukwuka Uba <promiseuba67@gmail.com>
2025-06-14 01:52:04 +0100

    fix: update all SquadSafeVault constructor calls for new signature; enhance deployment script for secure, flexible parameterization

--- script/DeploySquadSafeVault.s.sol
+++ script/DeploySquadSafeVault.s.sol
@@ -11,15 +11,25 @@ contract DeploySquadSafeVault is Script {
     function setUp() public pure {}
 
     function run() public {
-        // Use environment variable for owner, fallback to msg.sender
+        // Use environment variables for members, minVotes, and owner
+        address[] memory members = vm.envAddressArray("MEMBERS");
+        uint256 minVotes = vm.envOr("MIN_VOTES", uint256(1));
         address owner = vm.envOr("OWNER", msg.sender);
 
+        require(members.length > 0, "No members provided");
+        require(minVotes > 0, "minVotes must be > 0");
+
         vm.startBroadcast();
-        SquadSafeVault vault = new SquadSafeVault(owner);
+        SquadSafeVault vault = new SquadSafeVault(members, minVotes, owner);
         vm.stopBroadcast();
 
         // Log deployed address for easy reference
         console2.log("SquadSafeVault deployed at:", address(vault));
         console2.log("Owner:", owner);
+        console2.log("Members:");
+        for (uint256 i = 0; i < members.length; i++) {
+            console2.log(members[i]);
+        }
+        console2.log("minVotes:", minVotes);
     }
 }
